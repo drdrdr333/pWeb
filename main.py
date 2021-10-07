2212203cab2816105779c7ed5e2f869fc02c42b1
drdrdr333 <91769069+drdrdr333@users.noreply.github.com>
2021-10-06 21:29:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -86,6 +86,15 @@ def data():
     
         
     return jsonify(p1)
+
+
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
     
 
 

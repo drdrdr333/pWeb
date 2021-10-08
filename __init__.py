5feb4068bb72ecebcdd9247b652c429daebfe602
drdrdr333 <91769069+drdrdr333@users.noreply.github.com>
2021-10-07 23:02:10 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,9 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
 def init_app():
     """Construct the core application."""
     app = Flask(__name__, instance_relative_config=False)

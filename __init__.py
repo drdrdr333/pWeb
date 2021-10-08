c9a5d54a037c50605de524cbff3bbc186a8c8535
drdrdr333 <91769069+drdrdr333@users.noreply.github.com>
2021-10-07 22:55:27 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,18 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+"""Construct the core application."""
+app = Flask(__name__, instance_relative_config=False)
+app.config.from_object('config.Config')
 
+db.init_app(app)
 
-def init_app():
-    """Construct the core application."""
-    app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object('config.Config')
-
-    db.init_app(app)
-
-    with app.app_context():
-        from . import routes  # Import routes
-        db.create_all()  # Create sql tables for our data models
-
-        return app
+db = SQLAlchemy(app)

87d185c5dd8dda2de5a63524431e766fcdcd5c43
drdrdr333 <91769069+drdrdr333@users.noreply.github.com>
2021-10-11 12:25:47 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,15 +4,19 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
+# CONSTRUCTING THE CORE APPLICATION TO MAKE IT COMPATIBLE WITH SQLALCHEMY
+
 def init_app():
-    """Construct the core application."""
+    
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.Config')
 
     db.init_app(app)
 
+# IMPORTS THE ROUTES FOR THE APP SO THE DATABASE WILL KNOW WHERE ROUTING IS COMING FROM/TO AND CREATES A READBALE TABLE BASED UPON THE MODEL WE CREATE IN THE APP
+    
     with app.app_context():
-        from . import routes  # Import routes
-        db.create_all()  # Create sql tables for our data models
+        from . import routes  
+        db.create_all()  
 
         return app
